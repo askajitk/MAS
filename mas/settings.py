abb70bf9da258a0890db01c00debb940051b324b
askajitk <askjaitk@gmail.com>
2025-10-23 17:03:31 +0530

    Update settings.py configuration

--- mas/settings.py
+++ mas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-f9nz^sjb_6urdl=6t(m2iv^067=yg67m)#aggwebuyxh$akyut
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['askajitk.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
